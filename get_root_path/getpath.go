d147458c7bdd6ff09b6ca4b8ec0560ca997885b9
Xiangyang Li <13622004447@163.com>
2018-06-01 14:24:11 +0800

    Modified get_root_path/getpath.go

--- get_root_path/getpath.go
+++ get_root_path/getpath.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	// 获取可执行文件的绝对路径
+	root, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(root)
 }
