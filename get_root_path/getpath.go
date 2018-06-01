7ff262279a833d419bfc9b4a1c90844db65beae0
Xiangyang Li <13622004447@163.com>
2018-06-01 14:41:32 +0800

    Modified get_root_path/getpath.go

--- get_root_path/getpath.go
+++ get_root_path/getpath.go
@@ -7,9 +7,15 @@ import (
 	"path/filepath"
 )
 
+// 获取可执行文件的绝对路径
 func main() {
-	// 获取可执行文件的绝对路径
-	root, err := filepath.Abs(filepath.Dir(os.Args[0]))
+
+	// 获取相对于当前工作目录的相对路径
+	root := filepath.Dir(os.Args[0])
+	fmt.Println(root)
+
+	// 根据相对路径获取绝对路径
+	root, err := filepath.Abs(root)
 
 	if err != nil {
 		log.Fatalln(err)
